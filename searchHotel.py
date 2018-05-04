e55f91ccaa4eb00707065bca953e8bd06bf548a4
zeoyzhaogithub <1401518843@qq.com>
2018-05-04 15:15:11 +0800

    删除多余的引用文件

--- searchHotel.py
+++ searchHotel.py
@@ -2,9 +2,6 @@
 # -*-coding: utf-8-*-
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import time
